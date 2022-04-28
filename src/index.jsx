b60c18e2fe5d140fd58cd8653b42fcc4d2149d98
exxnnonymous <sulavbista51@gmail.com>
2022-04-29 02:07:18 +0545

    ✨ configured sass, styles and components

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
+
+import "style/index.scss";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
